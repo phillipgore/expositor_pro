f5c7739b3a14cafdd567b592aad99967f5095995
Phillip Gore <phillipagore@me.com>
2022-03-26 18:51:22 -0500

    toolbar and sheet inteaction refactor

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -8,9 +8,7 @@
 
 <div class="wrapper {$settingsState.wrapperIsRecessed ? 'recess' : ''}">
 	<Toolbar _id={'toolbar'}/>	
-	<main>
-
-	</main>
+	<main></main>
 </div>
 
 {#each $sheetsSetup as sheet}
@@ -18,25 +16,42 @@
 {/each}
 		
 <style>
-	.wrapper {
-		position: relative;
-		min-height: 100vh;
-		background-color: var(--white);
-		border-radius: 0.0rem 0.0rem 0.0rem 0.0rem;
-		transition: min-height 200ms ease-in-out, margin 200ms ease-in-out, border-radius 200ms ease-in-out;
+	@media only screen and (min-width: 0px) {
+		.wrapper {
+			position: relative;
+			min-height: 100vh;
+			background-color: var(--white);
+			border-radius: 0.0rem 0.0rem 0.0rem 0.0rem;
+			transition: min-height 200ms ease-in-out, margin 200ms ease-in-out, border-radius 200ms ease-in-out;
+		}
+
+		.wrapper.recess {
+			min-height: calc(100vh - 1.4rem);
+			margin: 1.4rem;
+			border-radius: 1.4rem 1.4rem 0.0rem 0.0rem;
+			transition: min-height 300ms ease-in-out, margin 300ms ease-in-out, border-radius 300ms ease-in-out;
+		}
+
+		main {
+			display: flex;
+			flex-direction: row;
+			min-height: 100vh;
+			padding-top: 5.0rem;
+			padding-right: 1.0rem;
+			padding-bottom: 1.0rem;
+			padding-left: 1.0rem;
+			transition: padding-top 300ms ease-in-out;
+		}
+
+		.wrapper.recess main {
+			padding-top: 1.2rem;
+			transition: padding-top 300ms ease-in-out;
+		}
 	}
 
-	.wrapper.recess {
-		min-height: calc(100vh - 1.4rem);
-		margin: 1.4rem;
-		border-radius: 1.4rem 1.4rem 0.0rem 0.0rem;
-		transition: min-height 300ms ease-in-out, margin 300ms ease-in-out, border-radius 300ms ease-in-out;
-	}
-
-	main {
-		display: flex;
-		flex-direction: row;
-		min-height: 100vh;
-		padding-top: 4.5rem;
+	@media only screen and (min-width: 768px) {
+		main {
+			padding-top: 5.5rem;
+		}
 	}
 </style>
